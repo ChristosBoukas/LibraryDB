12c9ca928418a0513ad206497da25e57cca2c12c
Christos Boukas <Christosboukast@gmail.com>
2023-12-06 14:32:37 +0100

    Renamed loan-entity to transaction

--- LibraryDB/Models/Transaction.cs
+++ LibraryDB/Models/Transaction.cs
@@ -7,15 +7,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LibraryDB.Models
 {
-    internal class Loan
+    internal class Transaction
     {
-        public int Loan_id { get; set; }
+        public int Transaction_id { get; set; }
         public LoanCard LoanCard_id { get; set; }
         public Book Book_id { get; set; }
         public DateTime LoanDate { get; set; }
         public DateTime ReturnDate { get; set; }
 
-        public Loan()
+        public Transaction()
         {
             
         }
